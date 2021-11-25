4dad8da226622ff872fc91ef2aba6fd6f06c820c
Jaime <jaimecr72@gmail.com>
2021-11-25 19:03:38 +0000

    initial bd done

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Film, Place, Country, Scene, PhotoFilm, PhotoPlace, FavPlace, Comment
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,14 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Film, db.session))
+    admin.add_view(ModelView(Place, db.session))
+    admin.add_view(ModelView(Country, db.session))
+    admin.add_view(ModelView(Scene, db.session))
+    admin.add_view(ModelView(PhotoFilm, db.session))
+    admin.add_view(ModelView(PhotoPlace, db.session))
+    admin.add_view(ModelView(FavPlace, db.session))
+    admin.add_view(ModelView(Comment, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    category = db.Column(db.Boolean(), unique=False, nullable=False)
+    #false for user, true for admin
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +19,141 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    director = db.Column(db.String(80), unique=True, nullable=False)
+    year = db.Column(db.Integer)
+    description = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Film %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idCountry = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    latitude = db.Column(db.Integer)
+    longitude = db.Column(db.Integer)
+    description = db.Column(db.String)
+    countLikes = db.Column(db.Integer)
+    entryDate = db.Column(db.Date)
+    country = db.relationship('Country')
+
+    def __repr__(self):
+        return '<Place %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+    
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    urlFlag = db.Column(db.String)
+    description = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Country %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Scene(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idFilm = db.Column(db.Integer, db.ForeignKey('film.id'), nullable=False)
+    idPlace = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
+    description = db.Column(db.String)
+    film = db.relationship('Film')
+    place = db.relationship('Place')
+    
+    def __repr__(self):
+        return '<Scene %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+
+
+class FavPlace(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    idPlace = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
+    user = db.relationship('User')
+    place = db.relationship('Place')
+    
+    def __repr__(self):
+        return '<FavPlace %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    idPlace = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
+    description = db.Column(db.String)
+    time = db.Column(db.Date)
+    user = db.relationship('User')
+    place = db.relationship('Place')
+    
+    def __repr__(self):
+        return '<FavPlace %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+
+class PhotoFilm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idFilm = db.Column(db.Integer, db.ForeignKey('film.id'), nullable=False)
+    urlPhoto = db.Column(db.String)
+    description = db.Column(db.String)
+    film = db.relationship('Film')
+    
+    def __repr__(self):
+        return '<PhotoFilm %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+
+class PhotoPlace(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idPlace = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
+    urlPhoto = db.Column(db.String)
+    description = db.Column(db.String)
+    place = db.relationship('Place')
+    
+    def __repr__(self):
+        return '<PhotoPlace %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
         }
