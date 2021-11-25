b069fb769b66752d5eded2b6fcf479638bb578ca
Jaime <jaimecr72@gmail.com>
2021-11-25 20:15:20 +0000

    initial db 2

--- src/api/models.py
+++ src/api/models.py
@@ -7,10 +7,9 @@ class User(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
     category = db.Column(db.Boolean(), unique=False, nullable=False)
     #false for user, true for admin
+    login = db.relationship("Login", uselist=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -20,6 +19,27 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
         }
+
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    lastTime = db.Column(db.Date)
+    user = db.relationship("User")
+    
+    def __repr__(self):
+        return '<Login %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+        }
+
+
+
 
 class Film(db.Model):
     id = db.Column(db.Integer, primary_key=True)
