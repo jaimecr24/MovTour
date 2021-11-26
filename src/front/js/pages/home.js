7059dbe9d23c0da45a438ac838259aa104e32342
Silvia <silviatmnez@gmail.com>
2021-11-26 21:45:24 +0000

    first home sketch

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,73 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import icon from "../../img/icon.png";
 import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const style = {
+		width: "200px"
+	};
+	const cardStyle = {
+		width: "15rem"
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>MovTour</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={icon} style={style} />
 			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<p>“The world isnt in your books and maps, it is out there.” ― The Hobbit, J.R.R. Tolkien</p>
+			<div className="row">
+				<div className="col-6 offset-3">
+					<div className="input-group">
+						<input
+							type="search"
+							className="form-control rounded"
+							placeholder="Search"
+							aria-label="Search"
+							aria-describedby="search-addon"
+						/>
+						<button type="button" className="btn btn-dark">
+							search
+						</button>
+					</div>
+				</div>
+			</div>
+			<br />
+			<div className="row">
+				<div className="col-3 offset-1">
+					<div className="card text-white bg-secondary mb-3" style={cardStyle}>
+						<div className="card-header">Sitios de Rodaje</div>
+						<div className="card-body">
+							<h5 className="card-title">Title</h5>
+							<img src="..." style={{ backgroundColor: "white", width: "100px", height: "100px" }} />
+						</div>
+					</div>
+				</div>
+
+				<div className="col-3 offset-1">
+					<div className="card text-white bg-secondary mb-3" style={cardStyle}>
+						<div className="card-header">Sitios de Rodaje</div>
+						<div className="card-body">
+							<h5 className="card-title">Title</h5>
+							<img src="..." style={{ backgroundColor: "white", width: "100px", height: "100px" }} />
+						</div>
+					</div>
+				</div>
+
+				<div className="col-3 offset-1">
+					<div className="card text-white bg-secondary mb-3" style={cardStyle}>
+						<div className="card-header">Sitios de Rodaje</div>
+						<div className="card-body">
+							<h5 className="card-title">Title</h5>
+							<img src="..." style={{ backgroundColor: "white", width: "100px", height: "100px" }} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
